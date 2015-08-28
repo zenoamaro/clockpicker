13663d025ee8ed58987807b6d73752bbf167d99c
Staffan Olsson <staffan@repos.se>
2015-08-28 11:21:23 +0200

    Adds callbacks from the callback example

--- duration-sample.js
+++ duration-sample.js
@@ -3,5 +3,48 @@ $('.clockpicker-duration').clockpicker({
 	placement: 'right',
 	twelveHour: true,
 	hourStep: 0.5,
-	minuteStep: 30
+	minuteStep: 30,
+
+  init: function() {
+    console.log("colorpicker initiated");
+  },
+  beforeShow: function() {
+    console.log("before show");
+  },
+  afterShow: function() {
+    console.log("after show");
+  },
+  beforeHide: function() {
+    console.log("before hide");
+  },
+  afterHide: function() {
+    console.log("after hide");
+  },
+  beforeHourSelect: function() {
+    console.log("before hour selected");
+  },
+  afterHourSelect: function() {
+    console.log("after hour selected");
+  },
+  beforeDone: function() {
+    console.log("before done");
+  },
+  afterDone: function() {
+    console.log("after done");
+  },
+  beforeClear: function() {
+    console.log("before clear");
+  },
+  afterClear: function() {
+    console.log("after clear");
+  },
+  beforeChange: function(time) {
+    console.log("before change: " + time);
+  },
+  afterChange: function(time) {
+    console.log("after change: " + time);
+  }
+})
+.find('input').change(function(){
+  console.log('duration value', this.value);
 });
